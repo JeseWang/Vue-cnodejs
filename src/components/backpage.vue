11d7329615d2f9ad9df38a776123759fdfba62c3
JesseWang <jessewang1024@gmail.com>
2017-11-08 17:37:52 +0800

    add backpage

---
+++ src/components/backpage.vue
@@ -0,0 +1,25 @@
+<template>
+<div class="iconfont icon-top" @click="goTop">&#xe611;</div>
+</template>
+<script>
+    export default {
+        replace: true,
+        methods: {
+            goTop() {
+                // $(window).scrollTop(0);
+                // this.show = false;
+                this.$router.go(-1);
+            }
+        }
+    };
+</script>
+<style>
+    .icon-top {
+        position: fixed;
+        right: 10px;
+        bottom: 80px;
+        font-size: 50px;
+        z-index: 9999;
+        color: #42b983;
+    }
+</style>
